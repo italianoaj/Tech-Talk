a9c6be2302b27fda6f4cff7aa6eea41f0761f766
Anthony Italiano <info@crossbonesprobramming.com>
2018-04-04 21:12:42 -0400

    Live feed messages

--- mong.js
+++ mong.js
@@ -7,46 +7,38 @@ var db;
 var bodyParser=require('body-parser');
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}));
+app.set('view engine', 'ejs');
 
 
 
 app.get('/', (req,res) => {
-	res.render('index.ejs');
+     db.collection('testMessages').find().toArray(function(err, results) {
+          res.render('index.ejs', {testMessages: results});
+    });
 });
 
-app.post('/testing', (req,res) => {
-	console.log(req);	
-});
 
 app.get('/messages', (req,res) => {
-    console.log("finish this shit");
+    console.log("Yes i see you asking for messages");
+     db.collection('testMessages').find().toArray(function(err, results) {
+          var obj = {testMessages: results};
+          res.send(JSON.stringify(obj));
+    });
 });
 
 app.post('/sendMessage', (req,res) => {
-    var suffix;
-    var prefix;
-    
-    time = new Date();
-    if(time.getMinutes()<10){
-        prefix="0";
-    }else{
-        prefix="";
-    }
-    if(time.getHours()<12){
-        suffix="am";
-    }else{
-        suffix="pm";
-    }
-    var sent=time.getHours()+":"+prefix+time.getMinutes()+""+suffix;
-    console.log(req.body);
-    var username=req.body.username;
-    var message=req.body.message;
-   	console.log("request made at "+time.getHours()+":"+prefix+time.getMinutes()+""+suffix);
-    db.collection('messages').save(req.body, (err, result)=> {
-        if(err) return console.log(err);
-        console.log("Saved data to italianoajDB");
-        var obj = {"time":sent,"username":username,"message":message};
-        res.send(JSON.stringify(obj));
+    db.collection('testMessages').save(req.body, (err, result) => {
+        if (err) return console.log(err)
+        console.log(req.body);
+        var username=req.body.username;
+        var message=req.body.message;
+        console.log("request made at "+req.body.time);
+        db.collection('messages').save(req.body, (err, result)=> {
+            if(err) return console.log(err);
+            console.log("Saved data to italianoajDB");
+            var obj = {"time":req.body.time,"username":username,"message":message};
+            res.send(JSON.stringify(obj));
+        });
     });
 });
 
@@ -68,9 +60,10 @@ app.post('/login', (req, res) => {
     var sent=time.getHours()+":"+prefix+time.getMinutes()+""+suffix;
     var username='admin';
     var password='Welcome123';
+    var buser='louis'
     console.log("login attempt made at "+sent);
     console.log(req.body);
-    if(req.body.username==username && req.body.password==password){
+    if((req.body.username==username && req.body.password==password) || (req.body.username==buser && req.body.password==password)){
         console.log("login attempt successful");
         var obj={"login":true};
         res.send(JSON.stringify(obj));
