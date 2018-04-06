c5331a08e1767a05cda2f441c9d58fe37a0cc0ee
Anthony Italiano <info@crossbonesprobramming.com>
2018-04-06 00:12:31 -0400

    Final update

--- mong.js
+++ mong.js
@@ -10,12 +10,11 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
 var users = [{"username":"admin", "password":"Welcome123", "type":"admin"}, {"username":"bowsera","password":"0000", "type":"user"}, {"username":"wojtowiczak","password":"0001", "type":"user"}, {"username":"italianoaj","password":"Ant@Ita1", "type":"user"}, {"username":"barbianbm","password":"0002", "type":"user"}, {"username":"rozalskiji","password":"0003", "type":"user"}, {"username":"whitekj","password":"0004", "type":"user"}, {"username":"hendersonl","password":"0005", "type":"user"}, {"username":"oliphantlt","password":"0006", "type":"user"}, {"username":"ikedam","password":"0007", "type":"user"}, {"username":"bountsebese","password":"0008", "type":"user"}, {"username":"nouafowankosj","password":"0009", "type":"user"}, {"username":"topaliant","password":"0010", "type":"user"}, {"username":"martinjakozd","password":"0011", "type":"user"}, {"username":"test","password":"test","type":"test"}];
 
-
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
     res.render('index.ejs');
 });
 
-app.delete('/users', (req, res) =>{
+app.delete('/users', (req, res) => {
     users.splice(req.body.index, 1);
     res.send(JSON.stringify(users));
 });
@@ -23,6 +22,22 @@ app.delete('/users', (req, res) =>{
 app.post('/users', (req, res) => {
     var obj={"username":req.body.username, "password":req.body.password, "type":req.body.type};
     users.push(obj);
+    res.send(JSON.stringify(users));
+});
+
+app.put('/users', (req, res) => {
+    var changed=false;
+    for (var i=0; i<users.length; i++){
+        if(req.body.username==users[i].username){
+            changed=true;
+            users[i].password=req.body.password;
+        }
+    }
+    if(changed){
+        console.log(req.body.username+" password updated");
+    }else{
+        console.log(req.body.username+" password update failed");
+    }
     res.send(JSON.stringify(users));
 });
 
@@ -40,13 +55,12 @@ app.get('/messages', (req,res) => {
 app.post('/sendMessage', (req,res) => {
     db.collection('testMessages').save(req.body, (err, result) => {
         if (err) return console.log(err)
-        console.log(req.body);
         var username=req.body.username;
         var message=req.body.message;
         console.log("request made at "+req.body.time);
         db.collection('messages').save(req.body, (err, result)=> {
             if(err) return console.log(err);
-            console.log("Saved data to italianoajDB");
+            console.log("New message from "+username);
             var obj = {"time":req.body.time,"username":username,"message":message};
             res.send(JSON.stringify(obj));
         });
@@ -71,7 +85,7 @@ app.post('/login', (req, res) => {
     var sent=time.getHours()+":"+prefix+time.getMinutes()+""+suffix;
     for (var i=0; i<users.length; i++){
         if(req.body.username==users[i].username && req.body.password==users[i].password){
-            console.log("login attempt successful");
+            console.log(req.body.username+" login attempt successful");
             if(users[i].type=="admin"){
                 var obj={"login":true, "admin":true};
                 res.send(JSON.stringify(obj));
